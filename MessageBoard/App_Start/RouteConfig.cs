21f69dbe5e2f05910b89ea5523880437ac0a7224
andrei-semianenka <andrei.semianenka@gmail.com>
2015-05-20 15:26:44 +0300

    Set SendMessage default action of application. Set defines for Release configuration. Add debug output for what defines values where set during build.

--- MessageBoard/App_Start/RouteConfig.cs
+++ MessageBoard/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MessageBoard
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "SendMessage", id = UrlParameter.Optional }
             );
         }
     }
